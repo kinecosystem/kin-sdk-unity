45a2fe781f04dd1e2afca3779c6fb2d6ba2d9272
Mike <mike@prime31.com>
2019-02-08 18:12:55 -0800

    ios native code

---
+++ Kin Unity/Assets/Kin/Editor/NativeCode/iOS/KinBinding.m
@@ -0,0 +1,161 @@
+#import "KinPlugin-Generated-Swift.h"
+
+#define MakeStringCopy( _x_ ) ( _x_ != NULL && [_x_ isKindOfClass:[NSString class]] ) ? strdup( [_x_ UTF8String] ) : NULL
+#define GetStringParam( _x_ ) ( _x_ != NULL ) ? [NSString stringWithUTF8String:_x_] : [NSString stringWithUTF8String:""]
+#define GetStringParamOrNil( _x_ ) ( _x_ != NULL && strlen( _x_ ) ) ? [NSString stringWithUTF8String:_x_] : nil
+
+
+void _kinCreateClient( const char* clientId, int environment, const char* apiKey, const char* storeKey )
+{
+	[KinPlugin.instance createClientWithClientId:GetStringParam( clientId )
+									 environment:environment
+										  apiKey:GetStringParam( apiKey )
+										storeKey:GetStringParamOrNil( storeKey )];
+}
+
+
+void _kinFreeCachedClient( const char* clientId )
+{
+	[KinPlugin.instance freeCachedClientWithClientId:GetStringParam( clientId )];
+}
+
+
+const char* _kinImportAccount( const char* clientId, const char* accountId, const char* exportedJson, const char* passphrase )
+{
+	NSString* res = [KinPlugin.instance importAccountWithClientId:GetStringParam( clientId )
+														accountId:GetStringParam( accountId )
+													 exportedJson:GetStringParam( exportedJson )
+													   passphrase:GetStringParam( passphrase )];
+	return MakeStringCopy( res );
+}
+
+
+int _kinGetAccountCount( const char* clientId )
+{
+	return (int)[KinPlugin.instance getAccountCountWithClientId:GetStringParam( clientId )];
+}
+
+
+const char* _kinAddAccount( const char* clientId, const char* accountId )
+{
+	NSString* res = [KinPlugin.instance addAccountWithClientId:GetStringParam( clientId )
+													 accountId:GetStringParam( accountId )];
+	return MakeStringCopy( res );
+}
+
+
+bool _kinGetAccount( const char* clientId, const char* accountId, int index )
+{
+	return [KinPlugin.instance getAccountWithClientId:GetStringParam( clientId )
+											accountId:GetStringParam( accountId )
+												index:index];
+}
+
+
+const char* _kinDeleteAccount( const char* clientId, int index )
+{
+	NSString* res = [KinPlugin.instance deleteAccountWithClientId:GetStringParam( clientId ) index:index];
+	return MakeStringCopy( res );
+}
+
+
+void _kinClearAllAccounts( const char* clientId )
+{
+	[KinPlugin.instance clearAllAccountsWithClientId:GetStringParam( clientId )];
+}
+
+
+void _kinGetMinimumFee( const char* client )
+{
+	
+}
+
+
+void _kinFreeCachedAccount( const char* accountId )
+{
+	[KinPlugin.instance freeCachedClientWithClientId:GetStringParam( accountId )];
+}
+
+
+const char* _kinGetPublicAddress( const char* accountId )
+{
+	NSString* res = [KinPlugin.instance getPublicAddressWithAccountId:GetStringParam( accountId )];
+	return MakeStringCopy( res );
+}
+
+
+const char* _kinExport( const char* accountId, const char* passphrase )
+{
+	NSString* res = [KinPlugin.instance exportWithAccountId:GetStringParam( accountId ) passphrase:GetStringParam( passphrase )];
+	return MakeStringCopy( res );
+}
+
+
+void _kinGetStatus( const char* accountId )
+{
+	[KinPlugin.instance getStatusWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinGetBalance( const char* accountId )
+{
+	[KinPlugin.instance getBalanceWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinBuildTransaction( const char* accountId, const char* toAddress, const char* kinAmount, int fee, const char* memo )
+{
+//	[KinPlugin.instance sendTransactionWithAccountId:GetStringParam( accountId )
+//										   toAddress:GetStringParam( toAddress )
+//										   kinAmount:GetStringParam( kinAmount )
+//												memo:GetStringParamOrNil( memo )];
+}
+
+
+void _kinSendWhitelistTransaction( const char* accountId, const char* transactionId, const char* whitelist )
+{
+	
+}
+
+
+void _kinSendTransaction( const char* accountId, const char* transactionId )
+{
+	[KinPlugin.instance sendTransactionWithAccountId:GetStringParam( accountId )
+										   transactionId:GetStringParam( transactionId )];
+}
+
+
+void _kinAddPaymentListener( const char* accountId )
+{
+	[KinPlugin.instance addPaymentListenerWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinRemovePaymentListener( const char* accountId )
+{
+	[KinPlugin.instance removePaymentListenerWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinAddBalanceListener( const char* accountId )
+{
+	[KinPlugin.instance addBalanceListenerWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinRemoveBalanceListener( const char* accountId )
+{
+	[KinPlugin.instance removeBalanceListenerWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinAddAccountCreationListener( const char* accountId )
+{
+	[KinPlugin.instance addAccountCreationListenerWithAccountId:GetStringParam( accountId )];
+}
+
+
+void _kinRemoveAccountCreationListener( const char* accountId )
+{
+	[KinPlugin.instance removeAccountCreationListenerWithAccountId:GetStringParam( accountId )];
+}
---
+++ Kin Unity/Assets/Kin/Editor/NativeCode/iOS/KinPlugin.swift
@@ -0,0 +1,361 @@
+//
+//  KinPlugin.swift
+//  Unity-iPhone
+//
+//  Created by desaro on 11/2/18.
+//
+
+import Foundation
+import KinSDK
+
+
+struct Provider: ServiceProvider {
+	public let url: URL
+	public let network: Network
+	
+	static public let Test = Provider( url: URL( string: "http://horizon-testnet.kininfrastructure.com" )!, network: .testNet )
+	static public let Main = Provider( url: URL( string: "https://horizon-ecosystem.kininfrastructure.com" )!, network: .mainNet )
+	
+	
+	init( url: URL, network: Network ) {
+		self.url = url
+		self.network = network
+	}
+}
+
+
+@objc class KinPlugin : NSObject {
+	@objc static let instance = KinPlugin()
+	
+	var clients: Dictionary<String,KinClient> = Dictionary<String,KinClient>()
+	var accounts: Dictionary<String,KinAccount> = Dictionary<String,KinAccount>()
+	
+	var paymentListeners = Dictionary<String,PaymentWatch>()
+	var balanceListeners = Dictionary<String,BalanceWatch>()
+	var accountListeners = Dictionary<String,Promise<Void>>();
+	
+	let formatter = NumberFormatter()
+	
+	
+	override init()
+	{
+		formatter.generatesDecimalNumbers = true
+	}
+	
+	
+	private func unitySendMessage( method: String, param: String ) {
+		UnitySendMessage( "KinManager", method, param )
+	}
+	
+	
+	private func errorToJson( error: Error, accountId: String? ) -> String {
+		var dict = ["Message": "", "NativeType": "", "ErrorCode": NSNumber.init( value: 1 )] as [String : Any]
+		
+		if( accountId != nil ) {
+			dict["AccountId"] = accountId
+		}
+		
+		do {
+			let jsonData = try JSONSerialization.data( withJSONObject: dict, options: [] )
+			let jsonString = String( data: jsonData, encoding: .utf8 )
+			
+			return jsonString!
+		}
+		catch {
+			print( "Error converting error to JSON: \(error)" )
+			return "{}"
+		}
+	}
+	
+	
+	private func callbackToJson( accountId: String, value: String ) -> String {
+		let dict = ["AccountId": accountId, "Value": value]
+		
+		do {
+			let jsonData = try JSONSerialization.data( withJSONObject: dict, options: [] )
+			let jsonString = String( data: jsonData, encoding: .utf8 )
+			
+			return jsonString!
+		}
+		catch {
+			return "{}"
+		}
+	}
+	
+	
+	private func paymentInfoToJson( paymentInfo: PaymentInfo, accountId: String ) -> String {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+		
+		var dict = ["AccountId": accountId] as [String : Any]
+		dict["_Amount"] = formatter.string( from: paymentInfo.amount as NSDecimalNumber )
+		dict["CreatedAt"] = dateFormatter.string( from: paymentInfo.createdAt )
+		dict["DestinationPublicKey"] = paymentInfo.destination
+		dict["SourcePublicKey"] = paymentInfo.source
+		dict["Hash"] = paymentInfo.hash
+		dict["Memo"] = paymentInfo.memoText
+		
+		do {
+			let jsonData = try JSONSerialization.data( withJSONObject: dict, options: [] )
+			let jsonString = String( data: jsonData, encoding: .utf8 )
+			
+			return jsonString!
+		}
+		catch {
+			return "{}"
+		}
+	}
+	
+	
+	private func transactionToJson( transaction: TransactionEnvelope, accountId: String ) -> String {
+//		json.put( "Id", transaction.getId().id() );
+//		json.put( "WhitelistableTransactionPayLoad", transaction.getWhitelistableTransaction().getTransactionPayload() );
+//		json.put( "WhitelistableTransactionNetworkPassphrase", transaction.getWhitelistableTransaction().getNetworkPassphrase() );
+		
+		let wtf = try? JSONEncoder().encode(transaction)
+		print(String(data: wtf!, encoding: .utf8)!)
+		
+		var dict = ["AccountId": accountId] as [String : Any]
+		dict["Id"] = transaction.tx.seqNum.tostring()
+		dict["WhitelistableTransactionPayLoad"] = ""
+		dict["WhitelistableTransactionNetworkPassphrase"] = ""
+		
+		do {
+			let jsonData = try JSONSerialization.data( withJSONObject: dict, options: [] )
+			let jsonString = String( data: jsonData, encoding: .utf8 )
+			
+			return jsonString!
+		}
+		catch {
+			return "{}"
+		}
+	}
+	
+
+	// MARK: - KinClient
+	
+	@objc public func createClient( clientId: String, environment: Int, apiKey: String, storeKey: String = "" )
+	{
+		let provider = environment == 0 ? Provider.Test : Provider.Main
+
+		do {
+			let client = KinClient( provider: provider, appId: try AppId( apiKey ) )
+			self.clients[clientId] = client;
+		}
+		catch {
+			print( "could not create KinClient: \(error)" )
+		}
+	}
+
+
+	@objc public func freeCachedClient( clientId: String ) {
+		if let _ = self.clients[clientId] {
+			self.clients.removeValue( forKey: clientId )
+		}
+	}
+	
+	
+	@objc public func importAccount( clientId: String, accountId: String, exportedJson: String, passphrase: String ) -> String? {
+		do {
+			let kinAccount = try self.clients[clientId].importAccount(exportedJson, passphrase: passphrase)
+			self.accounts[accountId] = kinAccount
+			return "";
+		}
+		catch {
+			print( "KinAccount couldn't be imported: \(error)" )
+			return errorToJson( error: error, accountId: accountId )
+		}
+		return nil;
+	}
+	
+	
+	@objc public func getAccountCount( clientId: String ) -> Int {
+		return self.clients[clientId]?.accounts.count ?? -1
+	}
+	
+	
+	@objc public func addAccount( clientId: String, accountId: String ) -> String? {
+		if let client = self.clients[clientId] {
+			do {
+				let account = try client.addAccount()
+				self.accounts[accountId] = account
+			}
+			catch {
+				print( "KinAccount couldn't be added: \(error)" )
+				return errorToJson( error: error, accountId: accountId )
+			}
+		}
+		return nil
+	}
+	
+	
+	@objc public func getAccount( clientId: String, accountId: String, index: Int ) -> Bool {
+		if let client = self.clients[clientId] {
+			let account = client.accounts[index]
+			self.accounts[accountId] = account
+		}
+		return true
+	}
+	
+	
+	@objc public func deleteAccount( clientId: String, index: Int ) -> String? {
+		if let client = self.clients[clientId] {
+			let account = client.accounts[index]
+			
+			do {
+				try client.deleteAccount( at: index )
+				
+				// now, remove the account from our cache
+				var foundKey: String?
+				for key in self.accounts.keys {
+					let temp = self.accounts[key]
+					if temp?.publicAddress == account?.publicAddress {
+						foundKey = key
+						break
+					}
+				}
+				
+				if foundKey != nil {
+					self.accounts.removeValue( forKey: foundKey! )
+				}
+			}
+			catch {
+				print( "KinAccount couldn't be deleted: \(error)" )
+				return errorToJson( error: error, accountId: nil )
+			}
+		}
+		return nil
+	}
+	
+	
+	@objc public func clearAllAccounts( clientId: String ) {
+		if let client = self.clients[clientId] {
+			client.deleteKeystore()
+		}
+	}
+	
+	
+	// MARK: - KinAccount
+	
+	@objc public func freeCachedAccount( accountId: String ) {
+		if self.accounts.keys.contains( accountId ) {
+			self.accounts.removeValue( forKey: accountId )
+		}
+	}
+	
+	
+	@objc public func getPublicAddress( accountId: String ) -> String {
+		return (self.accounts[accountId]?.publicAddress)!
+	}
+	
+	
+	@objc public func export( accountId: String, passphrase: String ) -> String? {
+		if let account = self.accounts[accountId] {
+			do {
+				return try? account.export( passphrase: passphrase )
+			}
+			catch {
+				print( "KinAccount couldn't be exported: \(error)" )
+				return errorToJson( error: error, accountId: nil )
+			}
+		}
+		return nil
+	}
+	
+	@objc public func getStatus( accountId: String ) {
+		if let account = self.accounts[accountId] {
+			//account.status(completion:
+		}
+		unitySendMessage( method: "GetStatusSucceeded", param: "" )
+		unitySendMessage( method: "GetStatusFailed", param: errorToJson( error: NSError(), accountId: accountId ) )
+	}
+	
+	
+	@objc public func getBalance( accountId: String ) {
+		if let account = self.accounts[accountId] {
+			//account.balance(completion:
+		}
+		unitySendMessage( method: "GetBalanceSucceeded", param: "" )
+		unitySendMessage( method: "GetBalanceFailed", param: errorToJson( error: NSError(), accountId: accountId ) )
+	}
+	
+	
+	@objc public func buildTransaction( accountId: String, toAddress: String, kinAmount: String, UInt32 fee, memo: String! = nil ) {
+		if let account = self.accounts[accountId] {
+			//account.generateTransaction(completion:
+		}
+		unitySendMessage( method: "BuildTransactionSucceeded", param: "" )
+		unitySendMessage( method: "BuildTransactionFailed", param: errorToJson( error: NSError(), accountId: accountId ) )
+	}
+	
+	
+	@objc public func sendTransaction( accountId: String, id: String ) {
+		if let account = self.accounts[accountId] {
+			//account.status(completion:
+		}
+		unitySendMessage( method: "SendTransactionSucceeded", param: "" )
+		unitySendMessage( method: "SendTransactionFailed", param: errorToJson( error: NSError(), accountId: accountId ) )
+	}
+	
+	
+	// MARK: - KinAccount Listeners
+	
+	@objc public func addPaymentListener( accountId: String ) {
+		if let account = self.accounts[accountId] {
+			let watch = try? account.watchPayments(nil)
+			watch?.emitter.on(queue: .main, next: { [weak self] payment in
+				let paymentInfoJson = paymentInfoToJson( paymentInfo: payment, accountId: accountId)
+				unitySendMessage( method: "OnPayment", param: paymentInfoJson )
+			})
+			
+			self.balanceListeners[accountId] = watch
+		}
+	}
+	
+	
+	@objc public func removePaymentListener( accountId: String ) {
+		if self.paymentListeners.keys.contains( accountId ) {
+			self.paymentListeners.removeValue( forKey: accountId )
+		}
+	}
+	
+	
+	@objc public func addBalanceListener( accountId: String ) {
+		if let account = self.accounts[accountId] {
+			let watch = try? account.watchBalance(nil)
+			watch?.emitter.on(queue: .main, next: { [weak self] balance in
+				let callbackJson = callbackToJson(accountId: accountId, value: balance)
+				unitySendMessage( method: "OnBalance", param: callbackJson )
+			})
+			
+			self.balanceListeners[accountId] = watch
+		}
+	}
+	
+	
+	@objc public func removeBalanceListener( accountId: String ) {
+		if self.balanceListeners.keys.contains( accountId ) {
+			self.balanceListeners.removeValue( forKey: accountId )
+		}
+	}
+	
+	
+	@objc public func addAccountCreationListener( accountId: String ) {
+		if let account = self.accounts[accountId] {
+			let watch = try? account.watchCreation()
+			watch?.emitter.on(queue: .main, next: {
+				unitySendMessage( method: "OnAccountCreated", param: accountId )
+			})
+				
+			self.accountListeners[accountId] = watch
+		}
+	}
+	
+	
+	@objc public func removeAccountCreationListener( accountId: String ) {
+		if self.accountListeners.keys.contains( accountId ) {
+			self.accountListeners.removeValue( forKey: accountId )
+		}
+	}
+
+}
