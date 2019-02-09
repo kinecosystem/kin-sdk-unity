bdf33701b14065b029ebcc99ec8d7c1c01c9fe08
Mike <mike@prime31.com>
2019-02-08 18:12:47 -0800

    frameworks

---
+++ Kin Unity/Assets/Kin/Editor/NativeCode/iOS/KinSDK.framework/Headers/KinSDK.h
@@ -0,0 +1,18 @@
+//
+//  KinSDK.h
+//  KinSDK
+//
+//  Created by Kin Foundation
+//  Copyright © 2017 Kin Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KinSDK.
+FOUNDATION_EXPORT double KinSDKVersionNumber;
+
+//! Project version string for KinSDK.
+FOUNDATION_EXPORT const unsigned char KinSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KinSDK/PublicHeader.h>
+
