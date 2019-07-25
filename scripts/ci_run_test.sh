338418bd9f6c5fad5a0cd1f8b4751be39f607a38
Yossi Rizgan <yosriz@gmail.com>
2019-07-25 13:21:13 +0300

    CR fixes

--- scripts/ci_run_test.sh
+++ scripts/ci_run_test.sh
@@ -2,19 +2,24 @@
 set -x
 
 TEST_RESULT_FILE="/root/project/test_results.xml"
+# Google Pixel device with Android 9.0
+GENY_DEVICE_TEMPLATE_ID="107d757e-463a-4a18-8667-b8dec6e4c87e"
 
 # copy unity license file
 mkdir /root/.local/share/unity3d/Unity
 cp /root/project/Unity_license.ulf /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
-# login to gmsaas (genymotion tool), and create device instance for tests
+# login to gmsaas (genymotion tool)
 gmsaas auth login $GENY_USERNAME "$GENY_PASSWORD"
-instance_id=$(gmsaas instances start 107d757e-463a-4a18-8667-b8dec6e4c87e ci_test)
+# start the instance
+instance_id=$(gmsaas instances start $GENY_DEVICE_TEMPLATE_ID ci_test)
+# connect insatnce via ADB
 gmsaas instances adbconnect $instance_id
 
 # extract the port adb is connected to the remote device 
 adb_port=$(adb devices |grep localhost: | grep -o -E '[0-9]+')
-# use it 
+# WORKAROUND: expliclty forward adb device port to Unity test runner, from some reason Unity test runner
+# is failed doing that in the build/test process
 adb -s "localhost:$adb_port" forward "tcp:34999" "localabstract:Unity-com.UnityTestRunner.UnityTestRunner"
 
 # run unity test with remote device (use xvfb to create required virtual screen)
@@ -37,6 +42,6 @@ else
   cat $TEST_RESULT_FILE
 fi
 
-cat $TEST_RESULT_FILE |grep test-run | grep "result="
+grep test-run $TEST_RESULT_FILE | grep "result="
 gmsaas instances stop $instance_id
 exit $UNITY_EXIT_CODE
